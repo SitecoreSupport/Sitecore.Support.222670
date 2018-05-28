8105013a965691de63e77f8051ee35fa0248be54
DK\obr <olegbraginskyy@sitecore.net>
2018-05-28 13:09:42 +0300

    We allow to pick up the value from SV and FallBack

--- src/Sitecore.Support.222670/Marketing/Search/FieldReaders/NullableDateFieldReader.cs
+++ src/Sitecore.Support.222670/Marketing/Search/FieldReaders/NullableDateFieldReader.cs
@@ -12,7 +12,7 @@ namespace Sitecore.Support.Marketing.Search.FieldReaders
     public override object GetFieldValue(IIndexableDataField indexableField)
     {
       Field field = (Field)(indexableField as SitecoreItemDataField);
-      if ((field == null) || (field.GetValue(false, false) != null))
+      if ((field == null) || (field.GetValue(true, false, true, false, true) != null))
       {
         DateField field2 = FieldTypeManager.GetField(field) as DateField;
         if (field2 != null)
