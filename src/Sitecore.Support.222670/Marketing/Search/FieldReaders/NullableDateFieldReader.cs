485a24caa95b9e4ee5a1c172ac62fe7bf8298cd8
DK\obr <olegbraginskyy@sitecore.net>
2018-05-28 13:06:31 +0300

    Init Commit

---
+++ src/Sitecore.Support.222670/Marketing/Search/FieldReaders/NullableDateFieldReader.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.Data.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Marketing.Search.FieldReaders
+{
+  public class NullableDateFieldReader : Sitecore.ContentSearch.FieldReaders.FieldReader
+  {
+    public override object GetFieldValue(IIndexableDataField indexableField)
+    {
+      Field field = (Field)(indexableField as SitecoreItemDataField);
+      if ((field == null) || (field.GetValue(false, false) != null))
+      {
+        DateField field2 = FieldTypeManager.GetField(field) as DateField;
+        if (field2 != null)
+        {
+          return field2.DateTime;
+        }
+      }
+      return null;
+    }
+  }
+}
---
+++ src/Sitecore.Support.222670/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.222670")]
+[assembly: AssemblyProduct("Sitecore.Support.222670")]
+[assembly: ComVisible(false)]
